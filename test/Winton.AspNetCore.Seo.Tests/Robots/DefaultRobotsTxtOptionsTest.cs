d6b9876521845bea1d4cdea4333761885345c1c1
Matthew Thornton <m.thornton@winton.com>
2017-10-03 11:37:41 +0100

    Minimise package dependencies and update to latest versions.

--- test/Winton.AspNetCore.Seo.Tests/Robots/DefaultRobotsTxtOptionsTest.cs
+++ test/Winton.AspNetCore.Seo.Tests/Robots/DefaultRobotsTxtOptionsTest.cs
@@ -44,7 +44,7 @@ namespace Winton.AspNetCore.Seo.Tests.Robots
                 };
                 var defaultRobotsTxtOptions = new DefaultRobotsTxtOptions(hostingEnvironment);
 
-                var userAgentRecord = defaultRobotsTxtOptions.UserAgentRecords.Single();
+                UserAgentRecord userAgentRecord = defaultRobotsTxtOptions.UserAgentRecords.Single();
 
                 userAgentRecord.DisallowAll.Should().Be(expected);
             }
