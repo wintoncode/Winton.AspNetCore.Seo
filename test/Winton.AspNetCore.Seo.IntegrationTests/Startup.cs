f327d4182837cfdcf5a307297377496c3ee27ee1
Matthew Thornton <m.thornton@winton.com>
2017-10-11 16:43:47 +0100

    Add integration tests for SeoController.

---
+++ test/Winton.AspNetCore.Seo.IntegrationTests/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Winton.AspNetCore.Seo.Extensions;
+
+namespace Winton.AspNetCore.Seo.IntegrationTests
+{
+    internal class Startup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(builder => builder.MapSeoRoutes());
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddSeo<SitemapConfig>();
+            return services.BuildServiceProvider();
+        }
+    }
+}
